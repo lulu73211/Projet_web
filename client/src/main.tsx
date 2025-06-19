cfd796ed7cc59c90fd8d058355fb37effea9a154
Jules <jules.duarte@edu.esiee-it.fr>
2025-06-19 15:09:35 +0200

    le client en mieux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,25 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router";
-
-import './index.css'
-import App from './App.jsx'
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
     },
-    {
-        path: "/salut",
-        element: <div>Hello World</div>,
-    },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <RouterProvider router={router} />
   </StrictMode>,
