ad05c7655370222d26eb0ac115fe3d8d48cd306b
Jules <jules.duarte@edu.esiee-it.fr>
2025-06-19 14:51:44 +0200

    router basique

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router";
+
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/salut",
+        element: <div>Hello World</div>,
+    },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
