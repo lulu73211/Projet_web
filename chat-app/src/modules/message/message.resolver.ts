3cc2704ab3b50ab82702669174782dd1aff1fe91
Mario <mario.aboujamra@gmail.com>
2025-07-01 10:06:04 +0200

    ✨chat-app: add redis ws system

--- chat-app/src/modules/message/message.resolver.ts
+++ chat-app/src/modules/message/message.resolver.ts
@@ -1,11 +1,19 @@
-import { Resolver, Mutation, Args, Int, Query } from '@nestjs/graphql';
+import {
+  Resolver,
+  Mutation,
+  Args,
+  Int,
+  Query,
+  Subscription,
+} from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { Logger } from '@nestjs/common';
 import { RabbitProducer } from '../rabbitmq/producer.service';
 import { MessageService } from './message.service';
 import { MessageEntity } from './entities/message.model';
-import { SendMessageInput } from './dto/message.dto';
+import { MessagePayload, SendMessageInput } from './dto/message.dto';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { pubSub } from '../redis/pubsub.provider';
 
 @Resolver(() => MessageEntity)
 export class MessageResolver {
@@ -30,12 +38,20 @@ export class MessageResolver {
   async sendMessage(@Args('input') input: SendMessageInput) {
     this.logger.log(`Sending message: ${JSON.stringify(input)}`);
     try {
+      await pubSub.publish('message_send', { messageSend: input });
       await this.rabbit.publish('message_send', input);
-
       return true;
     } catch (error) {
       this.logger.error(`Error sending message: ${error}`, error.stack);
       throw error;
     }
+  }
+
+  @Subscription(() => MessagePayload, {
+    name: 'messageSend',
+    resolve: (payload) => payload.messageSend,
+  })
+  messageSend() {
+    return pubSub.asyncIterator('message_send');
   }
 }
